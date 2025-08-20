4c0fa88d78ef3fec24ef05a5dc8d2f7ed39d6aa9
KL2400030537 <2400030537@kluniversity.in>
2025-08-20 07:41:55 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,32 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import React, { Component } from 'react';
+import './Myapp.css';
 
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-import Greetings from '../Greetings';
-
-function App() {
-  const users = [
-    {name:'Raj Kumar'},
-    {name: 'Rani'}
-  ];
-  return (
-    <div>
-      <h1>Welcome to My App</h1>
-      <Greetings name="Jonathan"/>
-      <Greetings name="Mathew"/>
-    </div>
-  );
+class Myapp extends Component {
+    render() {
+        return (
+            <div className='myInfo'>
+                <img className='photo' src='/myphoto.png' alt='photo' />
+                <table>
+                    <tr>
+                        <td><label>Name</label></td>
+                        <td><label>M. Sai Sandeep</label></td>
+                    </tr>
+                    <tr>
+                        <td><label>Program</label></td>
+                        <td><label>B.Tech. - CSE</label></td>
+                    </tr>
+                    <tr>
+                        <td><label>Mobile No.</label></td>
+                        <td><label>1234567890</label></td>
+                    </tr>
+                    <tr>
+                        <td><label>Email ID</label></td>
+                        <td><label>padmanaban.k@kluniversity.in</label></td>
+                    </tr>
+                </table>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Myapp;
